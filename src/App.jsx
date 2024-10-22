4bd5ffc33a6a9180464ba4289bf962ed34ea1ab7
SanjayMuthuswamy <sanjay.muthuswamy1@gamil.com>
2024-10-22 14:47:18 +0530

    menubar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 
-
-
-
 const App = () => {
   return (
     <main className='overflow-x-hidden bg-white'>
---
+++ src/components/menubar/menubar.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import { MdMenu } from "react-icons/md";
+
+
