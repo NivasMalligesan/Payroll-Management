1d53df8b168da2ad6f8e3693cfbb2943f19afcab
NivasMalligesan <nivasmalligesan23@gmail.com>
2024-10-22 15:39:20 +0530

    new Features

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,13 +7,13 @@ const Hero = () => {
   return (
     <section className="relative overflow-hidden bg-light">
         <Navbar/>
-        <div className="container grid grid-cols-1 md:grid-cols-3 min-h-[650px]" >
+        <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[550px]" >
             {/*Info*/}
 
-            <div className='relative z-20 flex flex-col justify-center md:py-0'>
-                <div className='space-y-5 text-center md:text-left lg:max-w-[400]'>
-                    <h1 className="text-3xl lg:text-5xl font-bold !leading-snug">Global Payroll
-                    Platform Made For <span className='text-blue'> Employees</span></h1>
+            <div className='relative z-20 flex flex-col justify-center py-14 md:py-0'>
+                <div className='space-y-5 text-center md:text-left lg:max-w-[400] me-10'>
+                    <h1 className="text-6xl lg:text-6xl font-bold !leading-snug">Global Payroll <br />
+                    Platform Made For <br /> <span className='text-blue'> Employees</span></h1>
                     <p className='!leading-snug font-medium'>Easily automate payroll, taxes, and compliance with a seamless experience.</p>
                         <div className='flex justify-center md:justify-start '>
                             <button className='flex items-center gap-2 primary-button group'>
@@ -24,8 +24,8 @@ const Hero = () => {
                 </div>
             </div>
             {/* hero image*/}
-            <div className='flex items-center justify-center'>
-                <img src="{hero.png}" alt="" className='W-[400px]' />
+            <div className='flex items-center justify-end'>
+                <img src={hero} alt="" className=' rounded-lg W-[1000px] xl:w-[700px] relative z-10 drop-shadow' />
             </div>
         </div>
     </section>
